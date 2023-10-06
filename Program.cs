d1b05d8a3146cd2b27e24d66dac74a62c58d296d
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-10-06 12:44:59 +0530

    [Dhanya R Hegde] : Added InvoiceService

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CabInvoiceGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(Ridetype.Normal);
+            Console.WriteLine(invoiceGenerator.calculateFare(15.5, 25));
+        }
+    }
+}
