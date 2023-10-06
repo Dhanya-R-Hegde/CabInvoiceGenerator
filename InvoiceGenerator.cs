213ac8f28ab566022fed0ce25c6c71bed649b1a5
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-10-06 12:07:58 +0530

    [Dhanya R Hegde] : Added CalculateFareFunction

---
+++ InvoiceGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceGenerator
+    {
+        Ridetype rideType;
+        private RideRepository rideRepository;
+
+        //Constants
+        private readonly double MINIMUM_COST_PER_KM;
+        private readonly int COST_PER_TIME;
+        private readonly double MINIMUM_FARE;
+
+        /// <summary>
+        /// Constructor to Create RideRepository instance
+        /// </summary>
+
+        
+
+        //Function to Calculate Fare
+        public double calculateFare(double distance, int time)
+        {
+            double totalFare = 0;
+            try
+            {
+                //Calculating Total Fare
+                totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+            }
+            catch (CabInvoiceException)
+            {
+                if (rideType.Equals(null))
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.Invalid_Ride_Type, "Invalid ride type");
+                }
+                if (distance <= 0)
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.Invalid_Distance, "Invalid distance");
+                }
+                if (time < 0)
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.Invalid_Time, "Invalid time");
+
+                }
+            }
+            return Math.Max(totalFare, MINIMUM_FARE);
+        }
+    
+
+
+    }
+}
